516a657b3135ed826602a8f1c8907b627111f52e
Raimundo Medeiros <ychainstyle@gmail.com>
2025-04-26 13:11:26 -0300

    resolução Q16

---
+++ Unidade1/listaponteiros/questao16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+/*Identifique quais dos seguintes comandos é válido ou inválido*/
+int main() {
+/* float aloha[10], coisas[10][5], *pf, value = 2.2;
+    int i=3;
+    aloha[2] = value; // Vàlido. aloha tem 10 posições, a posição 2 recebe o valor de value que é um float.
+    scanf("%f", &aloha); // Inválido. tem que ser passado o endereço ded um elemento do array, não o endereço do array inteiro.
+    aloha = "value"; // Inválido. aloha é um array de floats, não pode receber uma string, além de que ele não pode ser atribuído diretamente.
+    printf("%f", aloha); // Inválido, %f espera um float e aloha é um array de floats, teria que passar um elemento do array.
+    coisas[4][4] = aloha[3]; // Vàlido, o array bidimenisonal coisas é um float e está recebendo um elemento do array aloha que é um float.
+    coisas[5] = aloha; // Inválido, não da pra atribuir um array inteiro a uma linha inteira dentro de um array bidimensional.
+    pf = value; // Inválido, pf é um ponteiro para float e value é um float, não pode atribuir diretamente.
+    pf = aloha; // Vàlido, pf é um ponteiro para float e aloha é um array de floats, então pf recebe o endereço do primeiro elemento do array aloha.
+*/
+
+}
